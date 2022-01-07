e68234cacb9b6ebdf6f8c4ca9381707d5171f65c
JoseCarlos <jocdiazmuic@gmail.com>
2022-01-07 18:32:14 -0500

    toggle theme and detect from browser

--- Reto-7-ConsumeApi/src/App.js
+++ Reto-7-ConsumeApi/src/App.js
@@ -1,14 +1,34 @@
+/* eslint-disable react/jsx-wrap-multilines */
 import './App.scss';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Center, Pagination } from '@mantine/core';
+import {
+  Center,
+  Pagination,
+  AppShell,
+  Header,
+  Image,
+  MantineProvider,
+  ColorSchemeProvider,
+} from '@mantine/core';
 import CharacterGrid from './components/CharacterGrid';
+import ToggleTheme from './components/ToggleTheme';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [activePage, setPage] = useState(2);
+  const [activePage, setPage] = useState(1);
   const [characters, setCharacters] = useState([]);
   const [pages, setPages] = useState();
+
+  const [colorScheme, setColorScheme] = useState('dark');
+  const toggleColorScheme = (value) => {
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  };
+  const onSelectMode = (mode) => {
+    setColorScheme(mode);
+    if (mode === 'dark') document.body.classList.add('dark-mode');
+    else document.body.classList.remove('dark-mode');
+  };
   useEffect(() => {
     const getCharacters = async () => {
       setLoading(true);
@@ -22,30 +42,85 @@ const App = () => {
     getCharacters();
   }, [activePage]);
 
+  useEffect(() => {
+    // Add listener to update styles
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', (e) => {
+        onSelectMode(e.matches ? 'dark' : 'light');
+      });
+
+    // Setup dark/light mode for the first time
+    onSelectMode(
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light',
+    );
+    // Remove listener
+    return () => {
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', () => {});
+    };
+  }, []);
+
   return (
-    <div>
-      <Center>
-        <Pagination
-          page={activePage}
-          onChange={setPage}
-          total={pages}
-          siblings={1}
-          style={{ marginBottom: 20 }}
-        />
-      </Center>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider theme={{ colorScheme }}>
+        <AppShell
+          padding='lg'
+          header={
+            <Header height={110} padding='md' width='auto'>
+              <ToggleTheme />
+              <Center>
+                <Image
+                  src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg'
+                  width={200}
+                  height='auto'
+                  margin='auto'
+                  alt='Random unsplash image'
+                />
+              </Center>
+            </Header>
+          }
+          styles={(theme) => ({
+            main: {
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+            },
+          })}
+        >
+          <div>
+            <Center>
+              <Pagination
+                page={activePage}
+                onChange={setPage}
+                total={pages}
+                siblings={1}
+                style={{ marginBottom: 20 }}
+              />
+            </Center>
 
-      <CharacterGrid characters={characters} loading={loading} />
+            <CharacterGrid characters={characters} loading={loading} />
 
-      <Center>
-        <Pagination
-          page={activePage}
-          onChange={setPage}
-          total={pages}
-          siblings={1}
-          style={{ marginTop: 20 }}
-        />
-      </Center>
-    </div>
+            <Center>
+              <Pagination
+                page={activePage}
+                onChange={setPage}
+                total={pages}
+                siblings={1}
+                style={{ marginTop: 20 }}
+              />
+            </Center>
+          </div>
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 };
 
--- Reto-7-ConsumeApi/src/index.js
+++ Reto-7-ConsumeApi/src/index.js
@@ -3,51 +3,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import { AppShell, Header, Image, MantineProvider, Title } from '@mantine/core';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MantineProvider theme={{ colorScheme: 'dark' }}>
-      <AppShell
-        padding='lg'
-        header={
-          <Header height={110} padding='md'>
-            <div
-              style={{
-                height: 100,
-                width: 'auto',
-                display: 'flex',
-                flexFlow: 'row wrap',
-                justifyContent: 'center',
-              }}
-            >
-              <Image
-                radius='md'
-                src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg'
-                width={200}
-                height='auto'
-                margin={0}
-                alt='Random unsplash image'
-              />
-              <Title order={4} align='center'>
-                Character List
-              </Title>
-            </div>
-          </Header>
-        }
-        styles={(theme) => ({
-          main: {
-            backgroundColor:
-              theme.colorScheme === 'dark'
-                ? theme.colors.dark[8]
-                : theme.colors.gray[0],
-          },
-        })}
-      >
-        <App />
-      </AppShell>
-    </MantineProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
