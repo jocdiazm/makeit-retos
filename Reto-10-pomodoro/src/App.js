0b5178648738314a6c98806d5a4c67bb64427c2b
JoseCarlos <jocdiazmuic@gmail.com>
2022-01-14 09:40:57 -0500

    basic structure and css

--- Reto-10-pomodoro/src/App.js
+++ Reto-10-pomodoro/src/App.js
@@ -1,9 +1,32 @@
+import gear from './images/gear.svg';
 import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Hola mundo</h1>
+    <div className='wrapper'>
+      <div className='ring'>
+        <svg width='518' height='518' viewBox='0 0 518 518'>
+          <circle strokeWidth='9px' x='0' y='y' cx='259' cy='259' r='254' />
+        </svg>
+      </div>
+
+      <div className='timer'>
+        <div className='time'>
+          <div className='minutes'>
+            <input type='text' value='15' disabled />
+          </div>
+          <div className='colon'>:</div>
+          <div className='seconds'>
+            <input type='text' value='00' disabled />
+          </div>
+        </div>
+        <button type='button' className='start'>
+          start
+        </button>
+        <button type='button' className='settings'>
+          <img src={gear} alt='Settings' />
+        </button>
+      </div>
     </div>
   );
 };
